43ad85eb8ee57959517afade3a21189c72f331a6
Cody Delzer <delzercody@gmail.com>
2023-04-11 17:45:55 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    // return list of drivers
+    const newlist = drivers.filter(drivers => drivers.toLowerCase() === string.toLowerCase())//arrow function
+
+    return newlist
+}
+
+function fuzzyMatch(drivers, string) {
+    const driversThatMatchFirstLetter = drivers.filter(drivers => drivers.startsWith(string))
+    return driversThatMatchFirstLetter
+}
+
+function matchName(drivers, string) {
+    const exactMatch = drivers.filter(drivers => drivers.name === string)
+    return exactMatch
+}
